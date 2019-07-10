6a857a4c93e4316574e5f7321494006e9fa6f794
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-07-10 10:49:31 +0200

    (service-normalization) up: extentions of service provider

--- api/services/normalization/src/ServiceProvider.ts
+++ api/services/normalization/src/ServiceProvider.ts
@@ -1,15 +1,24 @@
-import { Parents, Container } from '@ilos/core';
+import { Parents, Container, Interfaces, Extensions } from '@ilos/core';
 import { GeoProvider } from '@pdc/provider-geo';
+import { ConfigExtension } from '@ilos/config';
+import { ValidatorExtension, ValidatorMiddleware } from '@ilos/validator';
 
 import { NormalizationGeoAction } from './actions/NormalizationGeoAction';
 import { NormalizationTerritoryAction } from './actions/NormalizationTerritoryAction';
+import { normalizationGeoSchema } from './schemas/normalizationGeoSchema';
 
 @Container.serviceProvider({
-  providers: [GeoProvider],
-  handlers: [
-    NormalizationGeoAction,
-    NormalizationTerritoryAction,
-  ],
   config: __dirname,
+  handlers: [NormalizationGeoAction, NormalizationTerritoryAction],
+  providers: [GeoProvider],
+  middlewares: [['validate', ValidatorMiddleware]],
+  validator: [['normalizationGeo', normalizationGeoSchema]],
 })
-export class ServiceProvider extends Parents.ServiceProvider {}
+export class ServiceProvider extends Parents.ServiceProvider {
+  readonly extensions: Interfaces.ExtensionStaticInterface[] = [
+    ConfigExtension,
+    ValidatorExtension,
+    Extensions.Providers,
+    Extensions.Handlers,
+  ];
+}
